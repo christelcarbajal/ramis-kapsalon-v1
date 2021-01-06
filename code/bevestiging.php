02ef11ea4e3b6fd0099a83479d0d0a973c9728ea
Christel Carbajal <christelcarbajal@gmail.com>
2021-01-06 12:10:04 +0100

    files renamed

--- code/bevestiging.php
+++ code/bevestiging.php
@@ -7,7 +7,7 @@ $middlename = empty($_POST['middlename']) ? '' :  $_POST['middlename'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservering Geslaagd</title>
-    <link   rel="stylesheet" href="index-style.css">
+    <link   rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
 <h2>Reservering Geslaagd!</h2>
