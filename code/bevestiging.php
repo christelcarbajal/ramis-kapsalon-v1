8402e57a63ccc7965d144d320320963ee9cbd64b
Christel Carbajal <christelcarbajal@gmail.com>
2021-01-18 21:44:38 +0100

    Bevestigings pagina updated! (opmaak)

--- code/bevestiging.php
+++ code/bevestiging.php
@@ -1,31 +1,44 @@
 <?php
+$firstname = $_POST ['firstname'];
 $middlename = empty($_POST['middlename']) ? '' :  $_POST['middlename'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$date = $_POST['date'];
+$time = $_POST['time'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservering Geslaagd</title>
-    <link   rel="stylesheet" href="stylesheet.css">
-    <link rel="stylesheet" href="bevestiging-stylesheet.css"
+    <link rel="stylesheet" href="bevestiging-stylesheet.css">
 </head>
 <body>
-<h2>Reservering Geslaagd!</h2>
-
-<?php
-echo 'Voornaam:'.$_POST['firstname'].'<br>';
-if ($middlename!='') {
-    echo 'Tussenvoegsel:'.$_POST['middlename'].'<br>';
-}
-
-echo 'Achternaam:'.$_POST['lastname'].'<br>';
-echo 'E-mail:'.$_POST['email'].'<br>';
-echo 'Telefoon:'.$_POST['phone'].'<br>';
-
-$date = date ('d-m-Y' , strtotime($_POST ['date']));
-echo 'Datum:'.$date.'<br>';
-echo 'Tijd:'.$_POST['time'].'<br>';
-?>
+<div class="container">
+    <img src="../images/r-kapsalon-picture.jpg" alt="backgroundImage" width="960" height="720">
+    <div class="text-block">
+        <h2>Reservering Geslaagd!</h2>
+        <div>
+            <p>
+                Voornaam:&nbsp; <?= $firstname ?>
+                <br>
+                Tussenvoegsel:&nbsp; <?= $middlename ?>
+                <br>
+                Achternaam:&nbsp; <?= $lastname ?>
+                <br>
+                E-mail:&nbsp; <?= $email ?>
+                <br>
+                Telefoon:&nbsp; <?= $phone ?>
+                <br>
+                Datum:&nbsp; <?= $date ?>
+                <br>
+                Tijd:&nbsp; <?= $time ?>
+            </p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
